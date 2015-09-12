840212b133f639e7f94fc9825ffaeac099e11f56
Valerio Pierbattista <omegaiori@yahoo.it>
2015-09-12 12:51:54 +0200

    fix

--- php/index.php
+++ php/index.php
@@ -16,7 +16,6 @@ include 'css/above-the-fold.css.php';
 		<?php
 		include 'head.php';
 		?>
-	
 
 		<?php
 		include 'navigation.php';
@@ -26,17 +25,18 @@ include 'css/above-the-fold.css.php';
 			<div class="sub-container about">
 				<h1>Hello!</h1>
 
-				<h2>My name is Valerio Pierbattista and I have a passion for building things on the web.</h2>
-
-				<img src="images/dist/valeriopierbattista.png" class="profile-pic" data-src="images/dist/amazing.gif">
-				<p>
-					I am a <b>Front End Developer</b> and <b>Web Designer</b> living in  Rome, Italy. I love simple and functional
-					interfaces optimized for anything that can access the web. You can read more about me on my <a href="/cv" target="_blank"><i class="fa fa-file-text"></i> online résumé</a>.
-				</p>
-				<p>
-					I'm currently a Front-End Developer at <a href="http://doing.com" target="_blank">DOING</a>.
-				</p>
-
+				
+				<div class="personal-description-cont"> 
+					<img src="images/dist/valeriopierbattista.png" class="profile-pic" data-src="images/dist/amazing.gif">
+					<h2>My name is Valerio Pierbattista and I have a passion for building things on the web.</h2>
+					<p>
+						I am a <b>Front End Developer</b> and <b>Web Designer</b> living in  Rome, Italy. I love simple and functional
+						interfaces optimized for anything that can access the web. You can read more about me on my <a href="/cv" target="_blank"><i class="fa fa-file-text"></i> online résumé</a>.
+					</p>
+					<p>
+						I'm currently a Front-End Developer at <a href="http://doing.com" target="_blank">DOING</a>.
+					</p>
+				</div>
 				<br>
 				<h2><i class="fa fa-spotify" style="color:#90B436"></i> Listening on Spotify</h2>
 				<div class="now-playing"></div>
